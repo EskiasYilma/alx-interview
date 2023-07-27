91b32cb4343feeebb1c9d3a3bef3816e419faca7
exuszee <exuszee@gmail.com>
2023-07-27 19:45:00 +0300

    0

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+A function that returns pascals triangle
+"""
+
+from math import factorial
+
+
+def pascals_triangle(n):
+    """
+    A function that returns pascals triangle
+    """
+    if n <= 0:
+        return []
+    for i in range(n):
+        print("[", end="")
+        for j in range(i+1):
+            print(factorial(i)//(factorial(j)*factorial(i-j)),
+                  end="," if j < i else "")
+        print(']')

d00c2bac72a41461794b3815269dd3252eab7877
exuszee <exuszee@gmail.com>
2023-09-20 15:28:02 +0300

    0

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Given a pile of coins of different values,
+determine the fewest number of coins needed to meet a given amount
+"""
+
+
+def makeChange(coins, total):
+    """
+    Given a pile of coins of different values,
+    determine the fewest number of coins needed to meet a given amount
+    """
+    if total <= 0:
+        return 0
+
+    min_coins = [float('inf')] * (total + 1)
+    min_coins[0] = 0
+
+    for coin in coins:
+        for i in range(coin, total + 1):
+            min_coins[i] = min(min_coins[i], min_coins[i - coin] + 1)
+
+    if min_coins[total] == float('inf'):
+        return -1
+    else:
+        return min_coins[total]

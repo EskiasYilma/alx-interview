c0f620dd3199568d78ebe33432eb6cbf3cad11ff
exuszee <exuszee@gmail.com>
2023-10-03 17:34:12 +0300

    0

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,46 +4,48 @@ A function that returns name of the player that won the most rounds
 """
 
 
+def is_prime(num):
+    """
+    Checks for prime numbers
+    """
+    if num <= 1:
+        return False
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def game(n):
+    """
+    Create a set of all numbers from 2 to n and loop
+    """
+    remaining_numbers = set(range(2, n + 1))
+    maria_turn = True
+
+    while True:
+        prime_found = False
+        for num in range(2, n + 1):
+            if num in remaining_numbers and is_prime(num):
+                prime_found = True
+                remaining_numbers -= set(range(num, n + 1, num))
+                break
+
+        if not prime_found:
+            return "Ben" if maria_turn else "Maria"
+
+        maria_turn = not maria_turn
+
+
 def isWinner(x, nums):
     """
     A function that returns name of the player that won the most rounds
     """
-    def is_prime(num):
-        """
-        Check prime numbers
-        """
-        if num <= 1:
-            return False
-        for i in range(2, int(num**0.5) + 1):
-            if num % i == 0:
-                return False
-        return True
-
-    def play_game(n):
-        """
-        Create a set of all numbers from 2 to n
-        """
-        remaining_numbers = set(range(2, n + 1))
-        marias_turn = True
-
-        while True:
-            prime_found = False
-            for num in range(2, n + 1):
-                if num in remaining_numbers and is_prime(num):
-                    prime_found = True
-                    remaining_numbers -= set(range(num, n + 1, num))
-                    break
-
-            if not prime_found:
-                return "Ben" if marias_turn else "Maria"
-
-            marias_turn = not marias_turn
-
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        winner = play_game(n)
+        winner = game(n)
         if winner == "Maria":
             maria_wins += 1
         elif winner == "Ben":
